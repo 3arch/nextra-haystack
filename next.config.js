a470cbc56d686c0c7d11327f50e0afc9e38f6ba4
Shally Virk <svirk@novo.cloud>
2023-01-13 08:58:00 -0700

    Changes next config to transpile some pkgs so to avoid error at build time.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+const withNextra = require("nextra")({
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.jsx",
 })
- 
-module.exports = withNextra()
- 
+
+const config = {
+	experimental: {
+		transpilePackages: [
+			//  If needed, projects here
+			//  if you get the same error :)
+			// "haystack-core",
+			// "haystack-nclient",
+			// "haystack-react",
+			// "antd",
+			"@novo-int/novo-react",
+		],
+	},
+}
+
+module.exports = withNextra(config)
+
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
